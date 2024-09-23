6d963c69f193b9f8a1be99de2d521a39090c0481
Orimiya <1402151939@qq.com>
2024-09-23 10:50:39 +0800

    20240923

---
+++ src/main/java/com/xuecheng/orirpc/OriRpcApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.orirpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OriRpcApplication.class, args);
+    }
+
+}
